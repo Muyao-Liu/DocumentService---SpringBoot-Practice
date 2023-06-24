b7771d4e6bfd116e7000de8d6c52c71c8d3ab311
BrotherGua <53428603+Muyao-Liu@users.noreply.github.com>
2023-06-23 17:52:14 -0700

    Update the logics of service when using Jpa

--- src/main/java/com/documentService/documentService/Service/DocumentService.java
+++ src/main/java/com/documentService/documentService/Service/DocumentService.java
@@ -1,35 +1,71 @@
 package com.documentService.documentService.Service;
 
-import com.documentService.documentService.Dao.DocumentDao;
+import com.documentService.documentService.Dao.DocumentRepository;
 import com.documentService.documentService.Entity.Document;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class DocumentService {
-    @Autowired
-    DocumentDao documentDao;
 
-    public ResponseEntity<Document> getDocument(int id){
-        return documentDao.getDocument(id);
+    private final DocumentRepository documentRepository;
+
+    @Autowired
+    public DocumentService(DocumentRepository documentRepository){
+        this.documentRepository=documentRepository;
     }
 
-    public List<Document> listDocument(){
-        return documentDao.listDocument();
+    public ResponseEntity<Document> getDocument(int id){
+        try {
+            Optional<Document> documentOptional = documentRepository.findById(id);
+            return ResponseEntity.ok(documentOptional.get());
+        }catch(IllegalArgumentException e){
+            return ResponseEntity.notFound().build();
+        }
+    }
+
+    public ResponseEntity<List<Document>> listDocument(){
+        List<Document> documentOptional = documentRepository.findAll();
+        return ResponseEntity.ok(documentOptional);
     }
 
     public ResponseEntity<Document> createDocument(Document document){
-        return documentDao.createDocument(document);
+        try {
+            if (documentRepository.existsById(document.getId())) {
+                throw new IllegalArgumentException();
+            }
+            Document documentCreated=documentRepository.save(document);
+            return ResponseEntity.ok(documentCreated);
+        }catch (IllegalArgumentException e){
+            return ResponseEntity.status(HttpStatus.CONFLICT).build();
+        }
     }
 
     public ResponseEntity<Document> updateDocument(int id,String content){
-        return documentDao.updateDocument(id,content);
+        try {
+            Optional<Document> documentOptional = documentRepository.findById(id);
+            Document document=documentOptional.get();
+            document.setContent(content);
+            Document documentUpdated=documentRepository.save(document);
+            return ResponseEntity.ok(documentUpdated);
+        }catch (IllegalArgumentException e){
+            return ResponseEntity.notFound().build();
+        }
     }
 
     public ResponseEntity<Void> deleteDocument(int id){
-        return documentDao.deleteDocument(id);
+        try {
+            Optional<Document> documentOptional = documentRepository.findById(id);
+            documentRepository.deleteById(id);
+            return ResponseEntity.noContent().build();
+        }catch (IllegalArgumentException e){
+            return ResponseEntity.notFound().build();
+        }
     }
 }
